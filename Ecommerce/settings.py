0cb7e0453d06747572187db6d503ea016158b466
kevin bhanderi <kevinbhanderi66@gmail.com>
2021-01-28 10:21:02 +0530

    mysql db

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'Ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+       'ENGINE' : 'django.db.backends.mysql',
+       'NAME' : 'sp1' , # Name of the database
+       'USER': 'root', #user name for the database
+       'PASSWORD' : '', # password
+       'HOST' : 'localhost', # IP address of the database
+       'PORT' : '3306', # port number of database server
     }
 }
 
