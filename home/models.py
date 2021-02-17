e2495b3fcd316ef39bcd51f26cadfb16a592ad75
kevin bhanderi <kevinbhanderi66@gmail.com>
2021-02-17 18:13:59 +0530

    kevin

--- home/models.py
+++ home/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 
 # Create your models here.
-class User(models.Model):
-    name = models.CharField(max_length=100)
-    email=models.CharField(max_length=100)
-    dob = models.DateTimeField('date of birth')
+
+class Registration(models.Model):
+    name = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    dob=models.DateField()
+    email=models.CharField(max_length=50)
+    address=models.CharField(max_length=100)
     phone=models.CharField(max_length=10)
+    class Meta:
+        db_table = "account"
--- home/views.py
+++ home/views.py
@@ -5,28 +5,48 @@
 from django.views.generic import TemplateView
 from django.shortcuts import redirect
 from django.contrib import auth
+from django.contrib.auth import get_user_model
 from django.template.context_processors import csrf
+from home.models import Registration
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
+from django.db import connection
 from . import views
+
 def login(request):
     return render(request,'home/login.html')
 def index(request):
    return render(request,"home/base.html")
 def auth_view(request):
-    username = request.POST.get('username', '')
-    password = request.POST.get('password', '')
-    user = auth.authenticate(username=username,password=password)
-    if user is not None:
-        auth.login(request, user)
+    name=request.POST.get('username')
+    password=request.POST.get('password')
+    check=auth.authenticate(username=name,password=password)
+    if check is not None:
         return render(request,'home/loggedin.html')
     else:
         return render(request,'home/invalidlogin.html')
 def registration(request):
+    if(request.POST.get('name') and request.POST.get('password')):
+        saverecord = Registration()
+        saverecord.name = request.POST.get('name')
+        saverecord.password = request.POST.get('password')
+        saverecord.dob = request.POST.get('dob')
+        saverecord.email = request.POST.get('email')
+        saverecord.address = request.POST.get('address')
+        saverecord.phone = request.POST.get('phn')
+        saverecord.save()
+        name= request.POST.get('name')
+        User=get_user_model()
+        password=request.POST.get('password')
+        user=User.objects.create_user(username=name,password=password)
+        user.save()
+        return render(request,'home/login.html')
+    #messages.success(request,'Sorry some error occurred')
     return render(request,'home/registration.html')
 def loggedin(request):
     return render(request,'home/loggedin.html', {"full_name":request.user.username})
 def invalidlogin(request):
     return render(request,'home/invalidlogin.html')
 def logout(request):
-    auth.logout(request)
+    #auth.logout(request)
     return render(request,'home/logout.html')
